ca7775f1117dfaaf134a91e9b70ca93fbdf89490
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2018-11-23 17:41:01 +0200

    Initial Commit

---
+++ K Group Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  K Group Challenge
+//
+//  Created by Vladislav Kiryukhin on 23/11/2018.
+//  Copyright © 2018 Out East. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
