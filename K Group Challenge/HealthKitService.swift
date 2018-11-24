af28f98bf5c6f63f8bcc2a7cfb4f64e306d424f8
Filipp Fediakov <julik103@mail.ru>
2018-11-24 13:33:08 +0200

    Implement reading Mass, Height, Age, Sex from HealthKit

--- K Group Challenge/HealthKitService.swift
+++ K Group Challenge/HealthKitService.swift
@@ -9,6 +9,21 @@
 import Foundation
 import HealthKit
 
+struct HealthKitStaticData {
+    let age: Int
+    let gender: HKBiologicalSex
+}
+
+extension HKBiologicalSex {
+    var stringRepresentation: String {
+        switch self {
+        case .female: return "Female"
+        case .male: return "Male"
+        case .notSet, .other: return "Other"
+        }
+    }
+}
+
 enum HealthKitServiceError: Error {
     case notAvailableOnDevice
     case dataTypeNotAvailable
@@ -60,6 +75,117 @@ class HealthKitService {
                                                 (success, error) in
                                                 completion(success, error)
         }
+    }
+
+    func getAgeSexAndBloodType() throws -> HealthKitStaticData {
+            do {
+
+                //1. This method throws an error if these data are not available.
+                let birthdayComponents =  try store.dateOfBirthComponents()
+                let biologicalSex =       try store.biologicalSex()
+
+                //2. Use Calendar to calculate age.
+                let today = Date()
+                let calendar = Calendar.current
+                let todayDateComponents = calendar.dateComponents([.year],
+                                                                  from: today)
+                let thisYear = todayDateComponents.year!
+                let age = thisYear - birthdayComponents.year!
+
+                //3. Unwrap the wrappers to get the underlying enum values.
+                let unwrappedBiologicalSex = biologicalSex.biologicalSex
+
+
+                return HealthKitStaticData(age: age, gender: unwrappedBiologicalSex)
+            }
+    }
+
+    /// Returns most recorded recent height
+    ///
+    /// - Parameter completion: Double - Height in Metres
+    func getHeight(completion: @escaping(Double?, Error?) -> ()) {
+        //1. Use HealthKit to create the Height Sample Type
+        guard let heightSampleType = HKSampleType.quantityType(forIdentifier: .height) else {
+            print("Height Sample Type is no longer available in HealthKit")
+            return
+        }
+        getMostRecentSample(for: heightSampleType) { (sample, error) in
+            guard let sample = sample else {
+                if let error = error {
+                    completion(nil, error)
+                }
+                return
+            }
+
+
+            //2. Convert the height sample to meters, save to the profile model,
+            //   and update the user interface.
+            let heightInMeters = sample.quantity.doubleValue(for: HKUnit.meter())
+            completion(heightInMeters, error)
+        }
+    }
+
+    /// Returns most recorded recent mass
+    ///
+    /// - Parameter completion: Double - Mass in Kilos
+    func getMass(completion: @escaping(Double?, Error?) -> ()) {
+        //1. Use HealthKit to create the Height Sample Type
+        guard let massSampleType = HKSampleType.quantityType(forIdentifier: .bodyMass) else {
+            print("Mass Sample Type is no longer available in HealthKit")
+            return
+        }
+
+        getMostRecentSample(for: massSampleType) { (sample, error) in
+            guard let sample = sample else {
+                if let error = error {
+                    completion(nil, error)
+                }
+                return
+            }
+
+
+            //2. Convert the height sample to meters, save to the profile model,
+            //   and update the user interface.
+            let massInKilos = sample.quantity.doubleValue(for: HKUnit.gramUnit(with: .kilo))
+            completion(massInKilos, error)
+        }
+    }
+
+
+
+    private func getMostRecentSample(for sampleType: HKSampleType,
+                                   completion: @escaping (HKQuantitySample?, Error?) -> Swift.Void) {
+
+        //1. Use HKQuery to load the most recent samples.
+        let mostRecentPredicate = HKQuery.predicateForSamples(withStart: Date.distantPast,
+                                                              end: Date(),
+                                                              options: .strictEndDate)
+
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate,
+                                              ascending: false)
+
+        let limit = 1
+
+        let sampleQuery = HKSampleQuery(sampleType: sampleType,
+                                        predicate: mostRecentPredicate,
+                                        limit: limit,
+                                        sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+
+                                            //2. Always dispatch to the main thread when complete.
+                                            DispatchQueue.main.async {
+
+                                                guard let samples = samples,
+                                                    let mostRecentSample = samples.first as? HKQuantitySample else {
+
+                                                        completion(nil, error)
+                                                        return
+                                                }
+
+                                                completion(mostRecentSample, nil)
+                                            }
+        }
+
+        HKHealthStore().execute(sampleQuery)
     }
 }
 
