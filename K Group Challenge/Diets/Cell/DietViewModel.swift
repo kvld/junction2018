b950a9b227f6549b6d919c8ce74a2c90c54ba7b8
Vladislav Kiryukhin <kiryukhinv@gmail.com>
2018-11-24 13:38:56 +0200

    Diets table

---
+++ K Group Challenge/Diets/Cell/DietViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  DietViewModel.swift
+//  K Group Challenge
+//
+//  Created by Vladislav Kiryukhin on 24/11/2018.
+//  Copyright © 2018 Out East. All rights reserved.
+//
+
+import UIKit
+
+struct DietViewModel {
+    struct Ingredient {
+        let title: String
+        let quantity: String
+    }
+
+    let images: [UIImage]
+    let ingredients: [Ingredient]
+
+    let calories: String
+    let price: String
+}
